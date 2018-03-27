37d2a68f4295704cee9dddd50b1a2b9a8047cad4
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2018-03-27 17:38:23 +0200

    Use just complete - remove onSuccess from controllers

--- src/main/tv/codely/scala_http_api/entry_point/controller/user/UserPostController.scala
+++ src/main/tv/codely/scala_http_api/entry_point/controller/user/UserPostController.scala
@@ -2,8 +2,8 @@ package tv.codely.scala_http_api.entry_point.controller.user
 
 import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.StatusCodes.NoContent
-import akka.http.scaladsl.server.Directives.{onSuccess, complete}
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives.complete
+import akka.http.scaladsl.server.StandardRoute
 import tv.codely.scala_http_api.module.shared.user.domain.UserId
 import tv.codely.scala_http_api.module.user.application.register.UserRegistrar
 import tv.codely.scala_http_api.module.user.domain.UserName
@@ -11,8 +11,8 @@ import scala.concurrent.{ExecutionContext, Future}
 import cats.instances.future._
 
 final class UserPostController(registrar: UserRegistrar[Future])(implicit executionContext: ExecutionContext) {
-  def post(id: String, name: String): Route =
-    onSuccess(registrar.register(UserId(id), UserName(name))).tapply{
-      case _ => complete(HttpResponse(NoContent))
-    }
+  def post(id: String, name: String): StandardRoute =
+    complete(registrar.register(UserId(id), UserName(name)).map{
+      _ => HttpResponse(NoContent)
+    })
 }
--- src/main/tv/codely/scala_http_api/entry_point/controller/video/VideoPostController.scala
+++ src/main/tv/codely/scala_http_api/entry_point/controller/video/VideoPostController.scala
@@ -5,7 +5,7 @@ import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.StatusCodes.NoContent
 import akka.http.scaladsl.server.Route
 import tv.codely.scala_http_api.module.video.application.create.VideoCreator
-import akka.http.scaladsl.server.Directives.{onSuccess, complete}
+import akka.http.scaladsl.server.Directives.complete
 import tv.codely.scala_http_api.module.shared.user.domain.UserId
 import tv.codely.scala_http_api.module.video.domain.{VideoCategory, VideoDuration, VideoId, VideoTitle}
 import scala.concurrent.{ExecutionContext, Future}
@@ -13,7 +13,7 @@ import cats.instances.future._
 
 final class VideoPostController(creator: VideoCreator[Future])(implicit executionContext: ExecutionContext) {
   def post(id: String, title: String, duration: Duration, category: String, creatorId: String): Route =
-    onSuccess(creator.create(VideoId(id), VideoTitle(title), VideoDuration(duration), VideoCategory(category), UserId(creatorId))).tapply{
-      _ => complete(HttpResponse(NoContent))
-    }
+    complete(creator.create(VideoId(id), VideoTitle(title), VideoDuration(duration), VideoCategory(category), UserId(creatorId)).map{
+      _ => HttpResponse(NoContent)
+    })
 }
