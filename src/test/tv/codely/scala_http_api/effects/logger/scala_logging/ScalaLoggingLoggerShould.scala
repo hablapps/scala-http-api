8ca87e037d838667c3725100eac15fcbd8735c82
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2018-04-02 01:01:11 +0200

    refactoring (wip)

--- src/test/tv/codely/scala_http_api/effects/logger/scala_logging/ScalaLoggingLoggerShould.scala
+++ src/test/tv/codely/scala_http_api/effects/logger/scala_logging/ScalaLoggingLoggerShould.scala
@@ -1,4 +1,4 @@
-package tv.codely.scala_http_api.module.shared.infrastructure.logger.scala_logging
+package tv.codely.scala_http_api.effects.loggier.scala_logging
 
 import java.io.{File, PrintWriter, StringWriter}
 
