53e3621a813b4aef09f9ec0d0e3e9b7f6347da64
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2018-04-02 19:44:55 +0200

    Http4s impl

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,8 @@ object Dependencies {
   object Versions {
     val akka     = "2.5.9"
     val akkaHttp = "10.0.11"
+    val http4sVersion = "0.18.5"
+    val circeVersion = "0.9.1"
   }
 
   val production = Seq(
@@ -21,7 +23,14 @@ object Dependencies {
     "com.typesafe.scala-logging" %% "scala-logging"           % "3.7.2", // SLF4J Scala wrapper
     "net.logstash.logback"       % "logstash-logback-encoder" % "4.11", // Log JSON encoder
     "org.typelevel"              %% "cats-core"               % "1.0.1",
-    "org.typelevel"              %% "cats-effect"             % "0.10"
+    "org.typelevel"              %% "cats-effect"             % "0.10",
+    "org.http4s"                 %% "http4s-dsl"              % Versions.http4sVersion,
+    "org.http4s"                 %% "http4s-blaze-server"     % Versions.http4sVersion,
+    "org.http4s"                 %% "http4s-blaze-client"     % Versions.http4sVersion,
+    "org.http4s"                 %% "http4s-circe"            % Versions.http4sVersion,
+    "io.circe"                   %% "circe-generic"           % Versions.circeVersion,
+    "io.circe"                   %% "circe-literal"           % Versions.circeVersion,
+    "com.chuusai"                %% "shapeless"               % "2.3.3"
   )
 
   val test = Seq(
--- src/main/tv/codely/scala_http_api/package.scala
+++ src/main/tv/codely/scala_http_api/package.scala
@@ -2,15 +2,18 @@ package tv.codely
 
 package object scala_http_api{
 
-  import scala.concurrent.Future
-
   import cats.{Id, ~>}
+  import scala.concurrent.Future
+  import cats.effect.IO
+
   implicit def fromIdToFuture: Id ~> Future =
     λ[Id ~> Future](Future.successful(_))
 
-  import cats.effect.IO
   implicit def fromIOToFuture: IO ~> Future = 
     λ[IO ~> Future](io => io.unsafeToFuture)
+  
+  implicit def fromIdToIO: Id ~> IO =
+    λ[Id ~> IO](IO.apply(_))
 }
 
 package scala_http_api{
